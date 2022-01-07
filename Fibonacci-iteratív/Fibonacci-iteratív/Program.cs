c3ceb95f2c546c5835b382cfd3deb34cbed0445d
lukacsidori <96677537+lukacsidori@users.noreply.github.com>
2022-01-07 19:07:50 +0100

    Add files via upload
    Ez a verzió a Fibonacci számok iteratív számítását tartalmazza.

---
+++ Fibonacci-iteratív/Fibonacci-iteratív/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy egész számot: ");
+            int szam = int.Parse(Console.ReadLine());            
+            Console.WriteLine("----------------------------------");
+            
+            Fibonacci(szam);
+            
+            Console.ReadKey();
+
+        }
+
+        public static void Fibonacci(int n)
+        {
+            int result = 0;
+            int previous = 1;
+
+            Console.WriteLine("A Fibonacci elemek a következők: ");
+            Console.WriteLine(result);
+                
+            for (int i = 0; i < n-1; i++)
+            {
+                int temp = result;
+                result = previous;
+                Console.WriteLine(result);
+                previous = temp + previous;
+            }
+            
+        }
+
+
+    }
+}
